e03992e1be3f5e5d3a88f107e4957831a83202e5
nellybutera <buteranelly250@gmail.com>
2025-09-02 13:27:48 +0200

    database configurations completed

--- backend/settings.py
+++ backend/settings.py
@@ -78,12 +78,26 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# use this configuration for your project, if you don't have a mysql or own database configured.
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblogdb',
+        'USER': 'root',
+        'PASSWORD': 'tbna@2244',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
