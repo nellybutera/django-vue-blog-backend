ae0739326d222f7403af788b34049c532761c4af
nellybutera <buteranelly250@gmail.com>
2025-09-03 23:56:03 +0200

    define data models and serializers for blog posts and comments

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,1 +1,20 @@
 # This is a very important file. It will help me translate complex django models into native python data types that can be rendered into JSON/XML for my frontend integration.
+
+from rest_framework import serializers
+from .models import Post, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source="author.username")
+
+    class Meta:
+        model = Comment
+        fields = ["id", "author", "content", "created_at"]
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source="author.username")
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ["id", "title", "content", "author", "created_at", "updated_at", "comments"]
