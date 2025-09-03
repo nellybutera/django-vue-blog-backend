42c8c3cea8eb7b4e1cf62c8c0523f96a07998e8c
nellybutera <buteranelly250@gmail.com>
2025-09-03 23:56:40 +0200

    implement URL routing and viewsets for posts and comments

--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,23 @@
 # This is where i will have all my logic for web requests and responses. 
 # Including everything in between. All functionalities will be defined here.
+
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by("-created_at")
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
