fa01a64d669efe9c755f68413412a5cee27144c0
nellybutera <buteranelly250@gmail.com>
2025-09-03 23:56:40 +0200

    implement URL routing and viewsets for posts and comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,2 +1,14 @@
 # this file is for my app's url configuration.
 # it will contain all the urls for my blog app.
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r"posts", PostViewSet)
+router.register(r"comments", CommentViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
