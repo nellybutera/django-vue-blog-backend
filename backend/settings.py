adc3b71b53ef7bd33252a5566be7165928c2f493
nellybutera <buteranelly250@gmail.com>
2025-09-08 20:21:26 +0200

    chore: update CI workflow and settings for database configuration

--- backend/settings.py
+++ backend/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# Read the environment variables, providing default values for local development
 SECRET_KEY = os.getenv('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't') # Convert string to boolean
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = []
 
